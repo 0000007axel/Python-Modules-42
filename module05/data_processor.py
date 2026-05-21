b1437379873695c60436684b953e116e8fe4e56a
Axl <axlramaroson25@gmail.com>
2026-05-21 23:42:32 +0300

    ygo

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -1,69 +1,49 @@
-#!usr/bin/env python3.13
+#!/usr/bin/env
 
 
-<<<<<<< HEAD
 from abc import ABC, abstractmethod
 from typing import Any
 
 
 class DataProcessor(ABC):
-
-    def __init__(self, data) -> None:
+    def __init__(self, data: list[Any] | Any):
         self.data = data
-
+    
     @abstractmethod
     def validate(self, data: Any) -> bool:
         pass
 
     @abstractmethod
-    def ingest(self) -> None:
+    def ingest(self, data: Any) -> None:
         pass
 
-    def output(self, data:Any) -> tuple[int, str]:
-        print(data)
-        return 
+    def output(self) -> tuple[int, str]:
+        ...
 
 
 class NumericProcessor(DataProcessor):
-    def __init__(self, data: float) -> None:
-        self.data = data
-        super().__init__(self.data)
+    def __init__(self, data: Any):
+        super().__init__(data)
 
-    def validate(self, data: float) -> bool:
-        state: bool = True
-        print(f" Trying to validate input '{self.data}': {state}")
-
-    def ingest(self, data) -> None:
+    def validate(self, data: Any) -> bool:
         ...
 
+    def ingest(self, data: Any) -> None:
+        ...
 
 
 class TextProcessor(DataProcessor):
     ...
-=======
-from abc import ABC
 
-
-class DataProcessor(ABC):
-    def __init__(self) -> None:
-        processed_data: list[Any] = []
-
-
-def main() -> None:
-    print("=== Garden Data Processor ===")
->>>>>>> 6dee27c85148b3bffa23ece489d111e6ac85b62b
 
 class LogProcessor(DataProcessor):
     ...
 
 
-
-def main():
+def main() -> None:
     print("=== Code Nexus - Data Processor ===\n")
-    print("Testing Numeric Processor...")
-    num = NumericProcessor(42)
-
-
+    print("Testing numeric processor...")
+    
 
 if __name__ == "__main__":
     main()
