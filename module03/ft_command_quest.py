a167e200525329634a2710e2284fdf9f0198cf53
Axl <axlramaroson25@gmail.com>
2026-06-05 19:09:57 +0300

    first exercice of module 3 done

--- module03/ft_command_quest.py
+++ module03/ft_command_quest.py
@@ -1,6 +1,14 @@
+import sys
+
 def main() -> None:
     print("=== Command Quest ===")
-
+    print(f"Program name: {sys.argv[0].split('/')[-1]}" )
+    if len(sys.argv) == 1:
+        print("No arguments provided!")
+    else:
+        for i in range(1, len(sys.argv)):
+            print(f"Argument {i}: {sys.argv[i]}")
+    print(f"Total arguments: {len(sys.argv)}")
 
 if __name__ == "__main__":
     main()
