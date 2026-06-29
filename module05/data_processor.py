22a46f6fe81af83440f7206f7cb4b2722abfda2a
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-06-29 23:08:43 +0300

    sleepy

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -1,47 +1,17 @@
-#!/usr/bin/env python3.13
-from abc import ABC, abstractmethod
-from typing import Any
+import abc
+import typing
 
 
-class DataProcessor(ABC):
-    def __init__(self, data: list[Any] | Any):
-        self.data = data
-    
-    @abstractmethod
-    def validate(self, data: Any) -> bool:
-        pass
-
-    @abstractmethod
-    def ingest(self, data: Any) -> None:
-        pass
-
-    def output(self) -> tuple[int, str]:
-        print("do smth")
-
-
-class NumericProcessor(DataProcessor):
-    def __init__(self, data: Any):
-        super().__init__(data)
-
-    def validate(self, data: Any) -> bool:
-        ...
-
-    def ingest(self, data: Any) -> None:
-        ...
+class DataProcessor(abc.ABC):
+    def __init__(self, data: list[int | float | list[int | float]]) -> None:
+        
 
 
 class TextProcessor(DataProcessor):
     ...
 
+class NumericProcessor(DataProcessor):
+    ...
 
 class LogProcessor(DataProcessor):
     ...
-
-
-def main() -> None:
-    print("=== Code Nexus - Data Processor ===\n")
-    print("Testing numeric processor...")
-    
-
-if __name__ == "__main__":
-    main()
