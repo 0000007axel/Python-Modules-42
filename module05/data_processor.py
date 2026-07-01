db91345662bb5dcfc19d46d6c4009eb5dc019f64
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-01 11:47:27 +0300

    text and numeric processors done

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -45,6 +45,8 @@ class TextProcessor(DataProcessor):
                 self.ingested_count += len(data)
                 for element in data:
                     self.ingested_data.append(element)
+        else:
+            raise ValueError("Got exception: Improper textual data")
 
 
 class NumericProcessor(DataProcessor):
@@ -55,31 +57,53 @@ class NumericProcessor(DataProcessor):
             return True
         elif isinstance(data, list):
             for element in data:
-                if not isinstance(element, str) and not isinstance(element, float):
+                if not isinstance(element, int) and not isinstance(element, float):
                     return False
             return True
         else:
             return False
 
-    def ingest(self, data: int | float | list[int | float]) -> None:
+    def ingest(self, data: int | float | list[int | float] | list[int] | list[float]) -> None:
         if self.validate(data):
             if isinstance(data, int) or isinstance(data, float):
                 self.ingested_count += 1
-                self.ingested_data.append(data)
+                self.ingested_data.append(str(data))
             elif isinstance(data, list):
                 self.ingested_count += len(data)
                 for element in data:
-                    self.ingested_data.append(element)
+                    self.ingested_data.append(str(element))
+        else:
+            raise ValueError("Got exception: Improper numeric data")
     
-
-class LogProcessor(DataProcessor):
-    ...
-
-
-
+#Still need to make the validation first before ingestion
 
 def main() -> None:
     print("=== Code Nexus - Data Processor ===\n")
+    n = NumericProcessor()
+    t = TextProcessor()
+    numbers: list[int] = [1, 2, 3, 4, 5]
+    words: list[str] = ["Hello", "Nexus", "World"]
+    print(f"""Trying to validate input '42': {n.validate(42)}
+Trying to validate input 'Hello': {n.validate("Hello")}
+Test invalid ingestion of string 'foo' without prior validation:""")
+    try:
+        n.ingest("foo")
+    except ValueError as e:
+        print(e)
+    print(f"""Processing data: {numbers}
+Extracting 3 values...""")
+    n.ingest(numbers)
+    for i in range(3):
+        rank, value = n.output()
+        print(f"Numeric value {rank}: {value}")
+    print(f"""\n
+Testing Text Processor...
+Trying to validate input '42': {t.validate(42)}
+Processing data: {words}
+Extracting 1 value...""")
+    t.ingest(words)
+    t_rank, t_value = t.output()
+    print(f"Text value {t_rank}: {t_value}")
 
 
 if __name__ == "__main__":
