bb55629d35011d29c6c9888da310a1dca477890c
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-01 22:32:22 +0300

    exercice 0 complete

--- module05/data_processor.py
+++ module05/data_processor.py
@@ -4,7 +4,7 @@ from typing import Any
 
 class DataProcessor(ABC):
     def __init__(self) -> None:
-        self.ingested_data: list[Any] = []
+        self.ingested: list[Any] = []
         self.ingested_count: int = 0
         self.rank: int = 0
 
@@ -17,9 +17,9 @@ class DataProcessor(ABC):
         ...
 
     def output(self) -> tuple[int, str]:
-        outp = (self.rank, str(self.ingested_data[0]))
+        outp = (self.rank, str(self.ingested[0]))
         self.rank += 1
-        self.ingested_data.pop(0)
+        self.ingested.pop(0)
         return outp
         
 
@@ -40,11 +40,11 @@ class TextProcessor(DataProcessor):
         if self.validate(data):
             if isinstance(data, str):
                 self.ingested_count += 1
-                self.ingested_data.append(data)
+                self.ingested.append(data)
             elif isinstance(data, list):
                 self.ingested_count += len(data)
                 for element in data:
-                    self.ingested_data.append(element)
+                    self.ingested.append(element)
         else:
             raise ValueError("Got exception: Improper textual data")
 
@@ -67,22 +67,55 @@ class NumericProcessor(DataProcessor):
         if self.validate(data):
             if isinstance(data, int) or isinstance(data, float):
                 self.ingested_count += 1
-                self.ingested_data.append(str(data))
+                self.ingested.append(str(data))
             elif isinstance(data, list):
                 self.ingested_count += len(data)
                 for element in data:
-                    self.ingested_data.append(str(element))
+                    self.ingested.append(str(element))
         else:
             raise ValueError("Got exception: Improper numeric data")
     
-#Still need to make the validation first before ingestion
+class LogProcessor(DataProcessor):
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            for key, value in data.items():
+                if not isinstance(key, str) or not isinstance(value, str):
+                    return False
+            return True
+        elif isinstance(data, list):
+            for element in data:
+                if isinstance(element, dict):
+                    for key, value in element.items():
+                        if not isinstance(key, str) or not isinstance(value, str):
+                            return False
+                else:
+                    return False
+            return True
+        else:
+            return False
+
+    def ingest(self, data: dict[str, str] | list[dict[str, str]]) -> None:
+        if self.validate(data):
+            if isinstance(data, dict):
+                self.ingested_count += 1
+                self.ingested.append(f"{data['log_level']}: {data['log_message']}")
+            elif isinstance(data, list):
+                self.ingested_count += len(data)
+                for d in data:
+                    self.ingested.append(f"{d['log_level']}: {d['log_message']}")
+        else:
+            raise ValueError("Got exception: Improper log data")
+
 
 def main() -> None:
     print("=== Code Nexus - Data Processor ===\n")
     n = NumericProcessor()
     t = TextProcessor()
+    l = LogProcessor()
     numbers: list[int] = [1, 2, 3, 4, 5]
     words: list[str] = ["Hello", "Nexus", "World"]
+    logs: list[dict[str, str]] = [{'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+                                  {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}]
     print(f"""Trying to validate input '42': {n.validate(42)}
 Trying to validate input 'Hello': {n.validate("Hello")}
 Test invalid ingestion of string 'foo' without prior validation:""")
@@ -93,10 +126,10 @@ Test invalid ingestion of string 'foo' without prior validation:""")
     print(f"""Processing data: {numbers}
 Extracting 3 values...""")
     n.ingest(numbers)
-    for i in range(3):
+    for _ in range(3):
         rank, value = n.output()
         print(f"Numeric value {rank}: {value}")
-    print(f"""\n
+    print(f"""
 Testing Text Processor...
 Trying to validate input '42': {t.validate(42)}
 Processing data: {words}
@@ -104,7 +137,15 @@ Extracting 1 value...""")
     t.ingest(words)
     t_rank, t_value = t.output()
     print(f"Text value {t_rank}: {t_value}")
-
+    print(f"""
+Testing log processor...
+Trying to validate input 'Hello': {l.validate("Hello")}
+Processing data: {logs}
+Extracting 2 values...""")
+    l.ingest(logs)
+    for _ in range(2):
+        l_rank, l_value = l.output()
+        print(f"Log entry {l_rank}: {l_value}")
 
 if __name__ == "__main__":
     main()
