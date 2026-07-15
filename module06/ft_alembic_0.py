c5cef008050491d71046e17149c26e65cbb42392
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-15 14:13:18 +0300

    english club

---
+++ module06/ft_alembic_0.py
@@ -0,0 +1,5 @@
+import elements
+
+
+def test_fire() -> None:
+    print(f"Testing create_fire: {elements.create_fire()}")
---
+++ module06/test.py
@@ -0,0 +1,5 @@
+import alchemy
+
+
+print(f"test 1: {alchemy.create_air()}")
+print(f"test 2: {alchemy.create_earth()}")
