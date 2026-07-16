0ad5fa34506b26c7b80a35c4e65cf5512691ac8c
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-16 21:21:44 +0300

    [feat](./alchemy/potions.py): added the ft_distillation_0.py file

--- module06/alchemy/potions.py
+++ module06/alchemy/potions.py
@@ -1,10 +1,10 @@
-from .elements import create_earth
-from .elements import create_air
-import elements
+from .elements import create_earth, create_air
+from elements import create_fire, create_water
+
 
 def healing_potion() -> str:
-    return(f"Healing potion brewed with’[{create_earth()}]’ and ’[{create_air()}]")
+    return(f"Healing potion brewed with’{create_earth()}’ and ’{create_air()}’")
 
 
 def strength_potion() -> str:
-    return(f"Strength potion brewed with '[{elements.create_fire()}]' and '[{elements.create_water()}]'")
+    return(f"Strength potion brewed with '{create_fire()}' and '{create_water()}'")
