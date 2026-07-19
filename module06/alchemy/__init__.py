dfe1d427fa7c29bcb0a88917ad29cc2360f9499c
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-19 17:39:11 +0300

    [fix](/): restarted from scratch, done with first part

--- module06/alchemy/__init__.py
+++ module06/alchemy/__init__.py
@@ -1,5 +1,3 @@
 from .elements import create_air
-from .potions import strength_potion
-from .potions import healing_potion as heal
 
-print("imported the alchemy package")
+
--- module06/ft_alembic_1.py
+++ module06/ft_alembic_1.py
@@ -1,11 +1,8 @@
 from elements import create_water
 
 
-def test_water() -> None:
+if __name__ == "__main__":
     print(f"""=== Alembic 1 ===
 Using: 'from ... import ...' structure to access elements.py
-Testing create_water: {create_water()}\n""")
-
-
-if __name__ == "__main__":
-    test_water()
+Testing create_water: {create_water()}
+""")
