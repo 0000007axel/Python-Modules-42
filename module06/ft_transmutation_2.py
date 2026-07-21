09d7f40598d148ece173c1daeab8e6bd4f46ec07
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-21 16:42:45 +0300

    [fix](/alchemy/transmutation/): fixed import structure bug on transmutation 2

--- module06/ft_transmutation_2.py
+++ module06/ft_transmutation_2.py
@@ -4,4 +4,4 @@ import alchemy
 if __name__ == "__main__":
     print(f"""=== Transmutation 2 ===
 Import alchemy module only
-Testing lead to gold: {alchemy.transmutation.lead_to_gold()}""")
+Testing lead to gold: {alchemy.lead_to_gold()}""")
