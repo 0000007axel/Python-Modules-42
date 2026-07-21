d620b1ed784ead135da85ab2cbac16fcfb96518e
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-21 09:50:44 +0300

    [feat](/): finished a first version of the project, no norms yet

--- module06/ft_alembic_0.py
+++ module06/ft_alembic_0.py
@@ -1,8 +1,8 @@
 import elements
 
 if __name__ == "__main__":
-    print(f"""=== Alemic 0 ===
+    print(f"""=== Alembic 0 ===
 Using: 'import ...' structure to access elements.py
 Testing create_fire: {elements.create_fire()}
-          """)
+""")
 
--- module06/ft_kaboom_0.py
+++ module06/ft_kaboom_0.py
@@ -4,5 +4,5 @@ from alchemy import grimoire
 if __name__ == "__main__":
     print(f"""=== Kaboom 0 ===
 Using grimoire module directly
-Testing record light spell: {grimoire.light_spellbook.light_spell_record('Fantasy', 'ghjk')}
+Testing record light spell: {grimoire.light_spellbook.light_spell_record('Fantasy', 'Earth, wind and fire')}
 """)
