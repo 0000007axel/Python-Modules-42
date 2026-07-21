20c5c7a2cf7559b30a922df34a68b20f793d0575
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-21 18:36:22 +0300

    [fix](/): all fixes were made, norm okay

--- module06/ft_kaboom_0.py
+++ module06/ft_kaboom_0.py
@@ -1,8 +1,10 @@
-from alchemy.grimoire.light_spellbook import light_spell_record
+import alchemy.grimoire
 
 
 if __name__ == "__main__":
+    thing: str = alchemy.grimoire.light_spell_record('Fantasy',
+                                                     'Tea, haha and FiRe')
     print(f"""=== Kaboom 0 ===
 Using grimoire module directly
-Testing record light spell: {light_spell_record('Fanta', 'Tea, haha and FiRe')}
+Testing record light spell: {thing}
 """)
