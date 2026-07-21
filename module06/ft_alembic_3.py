b20aae62d518692065fd8f2ad74e3bd655705cf6
Axel Seth Ramaroson <seramaro@student.42antananarivo.mg>
2026-07-21 16:34:43 +0300

    [fix](/): fixed some of the import structures that did not match the subject requirements

--- module06/ft_alembic_3.py
+++ module06/ft_alembic_3.py
@@ -1,8 +1,8 @@
-from alchemy import elements
+from alchemy.elements import create_air
 
 
 if __name__ == "__main__":
     print(f"""=== Alembic 3 ===
 Accessing alchemy/elements.py using 'from ... import ...' structure
-Testing create_air: {elements.create_air()}
+Testing create_air: {create_air()}
 """)
