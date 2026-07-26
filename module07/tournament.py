e61a0d77b55f706a5b2b1956d608b2e8bf921cc8
Axl <sethaxel7000@gmail.com>
2026-07-26 15:23:37 +0300

    [feat]: updated battle function in tournament.py

--- module07/tournament.py
+++ module07/tournament.py
@@ -1,11 +1,29 @@
-from ex0 import FlameFactory, AquaFactory
+from ex0 import CreatureFactory, FlameFactory, AquaFactory
 from ex1 import HealingCreatureFactory, TransformCreatureFactory
-from ex2 import NormalStrategy, DefensiveStrategy, AggressiveStrategy
+from ex2 import BattleStrategy, NormalStrategy, DefensiveStrategy, AggressiveStrategy
+
+
+def battle(opponents: list[tuple[CreatureFactory, BattleStrategy]]):
+    for i in range(len(opponents)):
+        for j in range(i + 1, len(opponents)):
+            factory_a, strategy_a  = opponents[i]
+            factory_b, strategy_b = opponents[j]
+            creature_a = factory_a.create_base()
+            creature_b = factory_b.create_base()
+            print(f"""{creature_a.describe()}
+  vs.
+{creature_b.describe()}
+  now fight!
+{strategy_a.act(creature_a)}
+""")
+        
 
 
 if __name__ == "__main__":
-    flame_f: FlameFactory = FlameFactory()
-    aqua_f: AquaFactory = AquaFactory()
-
-    flame = flame_f.create_base()
-    aqua = aqua_f.create_base()
+    print(f"""Tournament 0 (basic)
+ [ (Flameling+Normal), (Healing+Defensive) ]""")
+    f_factory: FlameFactory = FlameFactory()
+    a_factory: AquaFactory = AquaFactory()
+    h_factory: HealingCreatureFactory = HealingCreatureFactory()
+    test1= [ (f_factory, NormalStrategy), (h_factory, DefensiveStrategy) ]
+    battle(test1)
