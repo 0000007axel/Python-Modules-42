dca3f96951fcf4fc485cd975adec76cf51374e0f
Axl <sethaxel7000@gmail.com>
2026-08-01 10:52:56 +0300

    [fix]: added the version checker function required by the subject

--- module08/ex1/loading.py
+++ module08/ex1/loading.py
@@ -1,25 +1,43 @@
 import importlib
+from importlib.metadata import version
+import sys
 
 
-def importer(modules: list[str]) -> None:
-    for module in modules:
-        try:
-            mod = importlib.import_module(module)
-            print(f"[OK] {module} ({mod.__version__}) - ", end="")
-            if module == "pandas":
-                print("Data manipulation ready")
-            elif module == "numpy":
-                print("Numerical computation ready")
-            elif module == "requests":
-                print("Newtork access ready")
-            elif module == "matplotlib":
-                print("Visualisation ready")
-            else:
-                print("Module imported successfully")
+def version_checker(package: str) -> str:
+    return version(package)
 
-        except ModuleNotFoundError:
-            print(f"[FAILURE] - Failed to import module '{module}'")
+
+def import_checker(module: str) -> bool:
+    module_description: str = 'Successfully imported the module'
+
+    if module == "pandas":
+        module_description = "Data manipulation ready"
+    elif module == "numpy":
+        module_description = "Numerical computation ready"
+    elif module == "requests":
+        module_description = "Newtork access ready"
+    elif module == "matplotlib":
+        module_description = "Visualisation ready"
+    else:
+        module_description = "Module imported successfully"
+    try:
+        importlib.import_module(module)
+        print(f"[OK] {module} ({version_checker(module)}) - " + module_description)
+        return True
+    except ModuleNotFoundError:
+        print(f"[FAILURE] - Failed to import module '{module}'")
+        return False
 
 
 if __name__ == "__main__":
-    importer(["pandas", "numpy", "requests", "matplotlib"])
+    mandatory = ["pandas", "numpy", "requests", "matplotlib"]
+    imported = []
+    for module in mandatory:
+        found = import_checker(module)
+        if found:
+            imported.append(module)
+    if mandatory not in imported:
+        sys.exit()
+
+        
+
