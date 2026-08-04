e4777fee6c0a4911bdc406ad6160526dada8d6b3
Axl <sethaxel7000@gmail.com>
2026-08-05 01:16:39 +0300

    [feat]: finished a first version of the ex0

--- module09/ex0/space_station.py
+++ module09/ex0/space_station.py
@@ -1,7 +1,8 @@
 from typing import Annotated
-from datetime import date, datetime
+from datetime import datetime
+
 try:
-    from pydantic import BaseModel, Field
+    from pydantic import BaseModel, Field, ValidationError
 except ModuleNotFoundError:
     print("""
 Could not import the pydantic module
@@ -19,3 +20,43 @@ class SpaceStation(BaseModel):
     last_maintenance: datetime
     is_operational: bool = True
     notes: Annotated[str | None, Field(max_length=200)]
+
+
+def main() -> None:
+    print(f"""
+Space Station Data Validation
+{'=' * 40}""")
+    try:
+        valid: SpaceStation = SpaceStation(station_id="ISS001",
+                                           name="International Space Station",
+                                           crew_size=6,
+                                           power_level=85.5,
+                                           oxygen_level=92.3,
+                                           last_maintenance=datetime.now(),
+                                           is_operational=True,
+                                           notes="")
+        print(f"""Valid station created:
+ID: {valid.station_id}
+Name: {valid.name}
+Crew: {valid.crew_size} people
+Power: {valid.power_level}%
+Oxygen: {valid.oxygen_level}%
+Status: {'Operational' if valid.is_operational else 'Non-Operational'}
+
+{'=' * 40}""")
+        invalid: SpaceStation = SpaceStation(station_id="S" * 7000,
+                                             name="International Space Station",
+                                             crew_size=50,
+                                             power_level=85.5,
+                                             oxygen_level=92.3,
+                                             last_maintenance=datetime.now(),
+                                             is_operational=True,
+                                             notes="")
+        print(invalid)
+    except ValidationError as e:
+        print("Caught validation error" + ("s" if len(e.errors()) > 1 else ""))
+        for error in e.errors():
+            print(error['loc'][0] + ": " + error['msg'])
+
+if __name__ == "__main__":
+    main()
